0d8b42ed4a881c1a52bb81a9c774eeca80edb4c0
jorgelucasalima <jorgelucasalima@gmail.com>
2021-04-07 19:02:04 -0300

    criação das routes.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const server = express();
+const routes = require("./routes")
 
 //habilitar arquivos statics-estaticos
 server.use(express.static("public"))
 
-
-//request, response
-server.get('/', (request, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
-
+//routes
+server.use(routes)
 
 server.listen(3000, () => console.log('rodando.'))
